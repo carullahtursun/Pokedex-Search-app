8928872f3f3bf11bc54265a5cba2d867631051d1
carullahtursun <62027425+carullahtursun@users.noreply.github.com>
2022-10-12 01:30:53 +0300

    poke-bg-colors

--- js/main.js
+++ js/main.js
@@ -2,6 +2,25 @@ const searchInput =document.querySelector("#poke-input");
 const searchBtn =document.querySelector(".btn-search");
 const pokeContainer =document.querySelector(".poke-container");
 
+const colors = {
+    fire: "#FDDFDF",
+    grass: "#DEFDE0",
+    electric: "#FCF7DE",
+    water: "#DEF3FD",
+    ground: "#f4e7da",
+    rock: "#d5d5d4",
+    fairy: "#fceaff",
+    poison: "#d6b3ff",
+    bug: "#f8d5a3",
+    dragon: "#97b3e6",
+    psychic: "#eaeda1",
+    flying: "#F5F5F5",
+    fighting: "#E6E0D4",
+    normal: "#F5F5F5",
+    ice: "#e0f5ff ",
+}
+
+
 const pokeCount =10;
 
 const initPokemon = async ()=>{
@@ -14,5 +33,32 @@ const getPokemon =async (id)=>{
     let url =`https://pokeapi.co/api/v2/pokemon/${id}`
     let res = await fetch(url);
     let data =await res.json();
-    console.log(data);
+    createPokemonBox(data);
 }
+
+const createPokemonBox=(pokemon)=>{
+    const name=pokemon.name[0].toUpperCase()+pokemon.name.slice(1);
+    const id=pokemon.id.toString().padStart(3,"0");
+    const weight =pokemon.weight;
+    const type =pokemon.types[0].type.name;
+    const color =colors[type];
+
+    const pokemonEl=document.createElement('div');
+    pokemonEl.classList.add('poke-box');
+    pokemonEl.innerHTML=`
+    
+    <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}" alt="">
+            <h4 class="poke-name">${name}</h4>
+            <p class="poke-id">${id}</p>
+            <p class="poke-weight">${weight}</p>
+            <p class="poke-type">Type: ${type}</p>
+    
+    `
+
+
+
+
+}
+
+
+initPokemon();
